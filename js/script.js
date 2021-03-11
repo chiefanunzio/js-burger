73c9de53ce6a87b6104bbbf5b6d533797095e09f
chiefanunzio <chiefa.nunzio@gmail.com>
2021-03-11 19:39:26 +0100

    ??

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
 // GOAL: sulla base di quanto visto a lezione, cercare di riprodurre il calcolo del prezzo del panino che parte da 10E, a cui vanno aggiunti i costi per gli ingredienti supplementari e va applicato un eventuale sconto in base al coupon.
 
 var calculate = document.getElementById('calculate');
-var totalprice = document.getElementById('total-price');
-// var ingredients = document.getElementsByClassName('ingredient');
-var checkboxs = document.getElementsByClassName('checkbox');
-var stockPrice = 10;
-
-
 calculate.addEventListener('click', function(){
   var nameBurger = document.getElementById('nome-panino').value;
 
   if(nameBurger.length < 1){
     alert('non hai inserito il nome');
   } else{
+    var stockPrice = 10;
+    var checkboxs = document.getElementsByClassName('checkbox');
 
+    for (var i = 0; i < checkboxs.length; i++) {
+      var checkbox = checkboxs[i];
+      var addCheckbox = checkbox.checked;
+      var priceCheckbox = parseInt(checkbox.data.price);
+
+      if(addCheckbox){
+        stockPrice += priceCheckbox
+      }
+      console.log(stockPrice);
+    }
 
   }
 
