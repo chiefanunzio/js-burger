cf3aa8815e8a51299f3da2138aebb65ea83421c0
chiefanunzio <chiefa.nunzio@gmail.com>
2021-03-11 17:29:43 +0100

    variabili

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-// GOAL: sulla base di quanto visto a lezione, cercare di riprodurre il calcolo del prezzo del panino che parte da 10E, a cui vanno aggiunti i costi per gli ingredienti supplementari e va applicato un eventuale sconto in base al coupon. Ricordo che per semplicita' potete cominciare dall'HTML/CSS condiviso, fino al completamento di tutte le funzioni js come viste a lezione; in caso di conclusione rapida e' possibile cercare di riprodurre la grafica originale, migliorando gli aspetti estetici.
+// GOAL: sulla base di quanto visto a lezione, cercare di riprodurre il calcolo del prezzo del panino che parte da 10E, a cui vanno aggiunti i costi per gli ingredienti supplementari e va applicato un eventuale sconto in base al coupon.
+
+var calculate = document.getElementById('calculate');
+var price = document.getElementById('total-price');
+var ingredients = document.getElementsByClassName('ingredient');
+var nameBurger = document.getElementById('nome-panino').value;
+var stockPrice = 10;
+var totalPrice = 0;
+
+calculate.addEventListner('click', function(){
+
+  if(nameBurger.lengthc  < 1){
+    alert('non hai inserito il nome');
+  } else{
+
+
+  }
+
+
+});
